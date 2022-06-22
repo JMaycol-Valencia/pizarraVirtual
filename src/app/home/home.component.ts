c080d54d0bb8dd72b498bfc25b2de5051b6311f2
JMaycol-Valencia <maycolvalencia50@gmail.com>
2022-06-22 18:26:29 -0400

    proyectoPizarra_cambio_04

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  lista:string[] = ["juan", "maycol", "pedro", "casandra", "camila", "vanesa"];
   constructor() { }
 
   ngOnInit(): void {
