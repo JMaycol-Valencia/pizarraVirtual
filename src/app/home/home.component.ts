523d7d01a547931f4d7ed3abff5f79584e6c90a9
maycolremoto <maycolvalencia50@gmail.com>
2022-06-23 11:35:59 -0400

    proyectoPizarra_cambio05

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   lista:string[] = ["juan", "maycol", "pedro", "casandra", "camila", "vanesa"];
+  lista02:string[] = ["carlos"];
+  vista:boolean = true;
   constructor() { }
+
+  cambio(){
+    this.vista != this.vista;
+    console.log(this.vista);
+  }
+
+
 
   ngOnInit(): void {
   }
